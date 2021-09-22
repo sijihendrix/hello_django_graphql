6a6471ad7100580188df21b334e6592c543448a3
Sij <sijibamgboye@yahoo.com>
2021-09-22 14:44:09 +0100

    Register models in Products/admin.py

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Book, Grocery
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(Grocery)
