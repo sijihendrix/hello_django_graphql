f6258ce054abac4d341e3fa6ae7abca257d6d429
Sij <sijibamgboye@yahoo.com>
2021-09-22 15:08:12 +0100

    register graphql route in urls.py and define a schema that uses the models we created

---
+++ ecommerce/products/schema.py
@@ -0,0 +1,58 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Category, Book, Grocery
+
+class CategoryType(DjangoObjectType):
+    class Meta: 
+        model = Category
+        fields = ('id','title')
+
+  
+class BookType(DjangoObjectType):
+    class Meta: 
+        model = Book
+        fields = (
+            'id',
+            'title',
+            'author',
+            'isbn',
+            'pages', 
+            'price',
+            'quantity', 
+            'description',
+            'imageurl',
+            'status',
+            'date_created',
+        )  
+
+class GroceryType(DjangoObjectType):
+    class Meta:
+        model = Grocery
+        fields = (
+            'product_tag',
+            'name',
+            'category',
+            'price',
+            'quantity',
+            'imageurl',
+            'status',
+            'date_created',
+        )
+
+class Query(graphene.ObjectType):
+    categories = graphene.List(CategoryType)
+    books = graphene.List(BookType)
+    groceries = graphene.List(GroceryType)
+
+    def resolve_books(root, info, **kwargs):
+        # Querying a list
+        return Book.objects.all()
+
+    def resolve_categories(root, info, **kwargs):
+        # Querying a list
+        return Category.objects.all()
+
+    def resolve_groceries(root, info, **kwargs):
+        # Querying a list
+        return Grocery.objects.all()
+schema = graphene.Schema(query=Query)
